8285f044abc47f8642ab405e17b2d5127dc39691
Technical Priyam <priyamvadofficial1@gmail.com>
2021-11-01 21:56:32 +0530

    implemented signin and sign out functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import ChannelListContainer from "./components/ChannelListContainer";
 import ChannelContainer from "./components/ChannelContainer";
+import Auth from "./components/Auth";
 const apiKey = "uzvg4g7k52gd";
 const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
     <div className="app__wrapper">
-      <Chat client={client} theme="team light">
+   { /*   <Chat client={client} theme="team light">
         <ChannelListContainer />
         <ChannelContainer />
-      </Chat>
+  </Chat> */}
+  <Auth/>
     </div>
   );
 }
